85c53afd5c46cead23170d31c17e1f3146bc9035
Zaryab ali <zaryabkhan248@gmail.com>
2026-05-15 23:22:13 +0500

    feat(utils): add ApiError class for structured error handling

---
+++ utils/apiError.js
@@ -0,0 +1,15 @@
+
+// apiError.js - used to throw errors in the controllers and handle them in the error middleware
+
+
+class ApiError extends Error{
+    constructor(statusCode, message,status='error'){
+    super(message);
+    this.statusCode = statusCode;
+    this.status = status
+    this.data=null;
+    this.message=message
+    }
+}
+
+export {ApiError}
